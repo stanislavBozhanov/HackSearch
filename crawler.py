469a0c59c397cd321cefda75850e4cfe0fe7bc4a
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-11-25 11:26:00 +0200

    crawler update

--- crawler.py
+++ crawler.py
@@ -1,22 +1,6 @@
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import requests
-
-url = 'teyadiya.com'
-base_url = 'http://teyadiya.com/'
-all_urls = []
-related_urls = []
-
-
-def get_href(all_links):
-    for line in all_links:
-        all_urls.append(line['href'])
-
-
-# it will ignore realtive links
-def get_internal_links(all_urls, url):
-    for link in all_urls:
-        if url in link:
-            related_urls.append(link)
 
 
 def get_a_tags(base_url):
@@ -26,16 +10,43 @@ def get_a_tags(base_url):
     return soup.find_all('a', href=True)
 
 
-def print_related_urls():
+def get_href(all_links):
+    all_urls = []
+    for line in all_links:
+        all_urls.append(line['href'])
+    return all_urls
+
+
+def prepare_link(url, href):
+    return urljoin(url, href)
+
+
+# it will ignore realtive links
+def get_related_links(all_hrefs, base_url):
+    related_urls = []
+    for link in all_hrefs:
+        new_link = prepare_link(base_url, link)
+        if url in new_link:
+            related_urls.append(url)
+    return related_urls
+
+
+def print_related_urls(related_urls):
     for url in related_urls:
         print(url)
 
 
 def main():
-    all_links = get_a_tags(base_url)
-    get_href(all_links)
-    get_internal_links(all_urls, url)
-    print_related_urls()
+    
+    url = 'hackbulgaria.com'
+    base_url = 'http://hackbulgaria.com/'
+    # all_urls = []
+    related_urls = []
+    a_tags = get_a_tags(base_url)
+    all_hrefs = get_href(a_tags)
+    related_links = get_related_links(all_hrefs, base_url)
+    print(related_links)
+
 
 if __name__ == '__main__':
     main()
