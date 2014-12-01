a6cde35c6ba8482131b9eeedd6a801fdcf7fd3b3
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-12-01 17:08:21 +0200

    crawler architecture changed to class

--- crawler.py
+++ crawler.py
@@ -3,50 +3,32 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_a_tags(base_url):
-    r = requests.get(base_url)
-    html = r.text
-    soup = BeautifulSoup(html)
-    return soup.find_all('a', href=True)
+class Cralwer:
 
+    def __init__(self, url):
+        self.url = url
+        self.home_url = 'http://' + url + '/'
+        self.crawl_queue = []
+        self.visited = []
 
-def get_href(all_links):
-    all_urls = []
-    for line in all_links:
-        all_urls.append(line['href'])
-    return all_urls
+    def get_a_tags(self, home_url):
+        r = requests.get(home_url)
+        html = r.text
+        soup = BeautifulSoup(html)
+        return soup.find_all('a', href=True)
 
+    def get_hrefs(self, all_a_tags):
+        all_hrefs = []
+        for a_tag in all_a_tags:
+            all_hrefs.append(a_tag['href'])
+        return all_hrefs
 
-def prepare_link(url, href):
-    return urljoin(url, href)
+    def filter_links(base_url, all_links, visited):
+        filtered_links = []
+        for link in all_links:
+            if base_url in link and link not in visited:
+                filtered_links.append(link)
+        return filtered_links
 
-
-# it will ignore realtive links
-def get_related_links(all_hrefs, base_url):
-    related_urls = []
-    for link in all_hrefs:
-        new_link = prepare_link(base_url, link)
-        if url in new_link:
-            related_urls.append(url)
-    return related_urls
-
-
-def print_related_urls(related_urls):
-    for url in related_urls:
-        print(url)
-
-
-def main():
-    
-    url = 'hackbulgaria.com'
-    base_url = 'http://hackbulgaria.com/'
-    # all_urls = []
-    related_urls = []
-    a_tags = get_a_tags(base_url)
-    all_hrefs = get_href(a_tags)
-    related_links = get_related_links(all_hrefs, base_url)
-    print(related_links)
-
-
-if __name__ == '__main__':
-    main()
+    def prepare_link(self, home_url, href):
+        return urljoin(home_url, href)
