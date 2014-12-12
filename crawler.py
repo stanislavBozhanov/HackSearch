cda97dd140c21a1ea0761b73a9443689b0c1822d
Stanislav Bozhanov <stanislav.bozhanov@gmail.com>
2014-12-12 18:55:18 +0200

    store data method

--- crawler.py
+++ crawler.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup, Doctype
 import requests
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from website import Website
 
 url = 'boredpanda.com'
 home_url = 'http://' + url + '/'
 engine = create_engine("sqlite:///hacksearch.db")
 session = Session(bind=engine)
+
 
 class Cralwer():
 
@@ -32,15 +34,23 @@ class Cralwer():
             return True
         return False
 
+    def soup_get_title(self, soup):
+        return soup.title.string
+
     def soup_get_page_description(self, soup):
             desc = soup.findAll(attrs={"name": "description"})
             return desc[0]['content']
 
-    def soup_get_title(self, soup):
-        return soup.title.string
-
     def store_data(self, url):
-        pass
+        soup = self.get_beautiful_soup(url)
+        title = self.soup_get_title(soup)
+        html = 4
+        if (self.soup_is_html_version_five(soup)):
+            html = 5
+        description = self.soup_get_page_description(soup)
+        data = Website(url, title, html, description)
+        session.add(data)
+        session.commit()
 
     def get_a_tags(self, soup):
         return soup.find_all('a', href=True)
@@ -68,18 +78,15 @@ class Cralwer():
         return self.crawl_queue.pop()
 
     def crawl(self, url, base_url):
-        print(url)
         self.visited.append(url)
-        #self.store_data(url)
+        self.store_data(url)
         soup = self.get_beautiful_soup(url)
         a_tags = self.get_a_tags(soup)
         all_hrefs = self.get_hrefs(a_tags)
         all_links = []
         for href in all_hrefs:
             all_links.append(self.prepare_link(self.home_url, href))
-        # print(all_links)
         all_links = self.filter_links(base_url, all_links, self.visited)
-        #print(all_links)
         self.crawl_queue.extend(all_links)
 
     def start(self):
@@ -88,15 +95,3 @@ class Cralwer():
             self.crawl(current_url, self.url)
         return self.crawled_data
         #self.crawl(self.get_next_craw())
-
-
-def main():
-    crawler = Cralwer(url, session)
-    soup = crawler.get_beautiful_soup(crawler.home_url)
-    if crawler.soup_is_html_version_five(soup)
-    print(crawler.soup_get_html_version(soup))
-    print(crawler.soup_get_title(soup))
-    print(crawler.soup_get_page_description(soup))
-
-if __name__ == '__main__':
-    main()
